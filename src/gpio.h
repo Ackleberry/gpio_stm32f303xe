c925c6cd455c840cda63d617866b7b644c61b1b3
Brooks Anderson <bilbrobaggins@gmail.com>
2020-07-21 18:40:01 -0400

    wip, first test pass

--- src/gpio.h
+++ src/gpio.h
@@ -8,6 +8,32 @@
 #ifndef _GPIO_H
 #define _GPIO_H
 
+#include <stdint.h>
 
+#include "stm32f303xe.h"
+
+typedef struct {
+   uint8_t port;
+   uint8_t pin;
+} Gpio_Pin_t;
+
+#define GPIO_PIN(port,pin) ((Gpio_Pin_t){ (port), (pin) })
+
+/*!< AHB2 peripherals */
+GPIO_TypeDef GPIO_BASE[8];
+#define GPIO ((GPIO_TypeDef *)GPIO_BASE)
+#define GPIO_PORT_A_IDX (0)
+#define GPIO_PORT_B_IDX (1)
+#define GPIO_PORT_C_IDX (2)
+#define GPIO_PORT_D_IDX (3)
+#define GPIO_PORT_E_IDX (4)
+#define GPIO_PORT_F_IDX (5)
+#define GPIO_PORT_G_IDX (6)
+#define GPIO_PORT_H_IDX (7)
+
+#define STM32F303_PIN_PA5    GPIO_PIN(GPIO_PORT_A_IDX, 5)
+
+#define GPIO_Set(p) (GPIO[(p).port].ODR |= (1 << (p).pin))
+#define GPIO_Clr(p) (GPIO[(p).port].ODR &= ~(1 << (p).pin))
 
 #endif /* _GPIO_H */
--- test/test_gpio.c
+++ test/test_gpio.c
@@ -1,5 +1,8 @@
 #include "unity.h"
 #include "gpio.h"
+#include "stm32f303xe.h"
+
+#define BIT_TO_MASK(a) (1u << (a))
 
 void setUp(void)
 {
@@ -10,7 +13,11 @@ void tearDown(void)
 {
 }
 
-void test_GPIO_firstTest(void)
+void test_GPIO_Set_ShouldSetOutputHigh(void)
 {
-   TEST_IGNORE_MESSAGE("Implement me!!!");
+   GPIO[GPIO_PORT_A_IDX].ODR = 0;
+
+   GPIO_Set(STM32F303_PIN_PA5);
+
+   TEST_ASSERT_EQUAL_HEX32(BIT_TO_MASK(5), GPIO[GPIO_PORT_A_IDX].ODR);
 }
