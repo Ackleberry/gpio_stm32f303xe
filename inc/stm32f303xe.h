2f54c3876d6ecc4fee5ec0acd3b8c38f161e4763
Brooks Anderson <bilbrobaggins@gmail.com>
2020-07-24 01:12:46 -0400

    Modify registers to exist in native RAM for testing

--- inc/stm32f303xe.h
+++ inc/stm32f303xe.h
@@ -22,4 +22,45 @@ typedef struct _GPIO_TypeDef
   __IO uint32_t BRR;          /*!< GPIO bit reset register,               Address offset: 0x28 */
 } GPIO_TypeDef;
 
+/*!< AHB2 peripherals */
+#ifdef TEST
+    GPIO_TypeDef GPIOA_BASE;
+    GPIO_TypeDef GPIOB_BASE;
+    GPIO_TypeDef GPIOC_BASE;
+    GPIO_TypeDef GPIOD_BASE;
+    GPIO_TypeDef GPIOE_BASE;
+    GPIO_TypeDef GPIOF_BASE;
+    GPIO_TypeDef GPIOG_BASE;
+    GPIO_TypeDef GPIOH_BASE;
+#else
+#   define GPIOA_BASE            (AHB2PERIPH_BASE + 0x00000000UL)
+#   define GPIOB_BASE            (AHB2PERIPH_BASE + 0x00000400UL)
+#   define GPIOC_BASE            (AHB2PERIPH_BASE + 0x00000800UL)
+#   define GPIOD_BASE            (AHB2PERIPH_BASE + 0x00000C00UL)
+#   define GPIOE_BASE            (AHB2PERIPH_BASE + 0x00001000UL)
+#   define GPIOF_BASE            (AHB2PERIPH_BASE + 0x00001400UL)
+#   define GPIOG_BASE            (AHB2PERIPH_BASE + 0x00001800UL)
+#   define GPIOH_BASE            (AHB2PERIPH_BASE + 0x00001C00UL)
+#endif
+
+#ifdef TEST
+#   define GPIOA               ((GPIO_TypeDef *) &GPIOA_BASE)
+#   define GPIOB               ((GPIO_TypeDef *) &GPIOB_BASE)
+#   define GPIOC               ((GPIO_TypeDef *) &GPIOC_BASE)
+#   define GPIOD               ((GPIO_TypeDef *) &GPIOD_BASE)
+#   define GPIOE               ((GPIO_TypeDef *) &GPIOE_BASE)
+#   define GPIOF               ((GPIO_TypeDef *) &GPIOF_BASE)
+#   define GPIOG               ((GPIO_TypeDef *) &GPIOG_BASE)
+#   define GPIOH               ((GPIO_TypeDef *) &GPIOH_BASE)
+#else
+#   define GPIOA               ((GPIO_TypeDef *) GPIOA_BASE)
+#   define GPIOB               ((GPIO_TypeDef *) GPIOB_BASE)
+#   define GPIOC               ((GPIO_TypeDef *) GPIOC_BASE)
+#   define GPIOD               ((GPIO_TypeDef *) GPIOD_BASE)
+#   define GPIOE               ((GPIO_TypeDef *) GPIOE_BASE)
+#   define GPIOF               ((GPIO_TypeDef *) GPIOF_BASE)
+#   define GPIOG               ((GPIO_TypeDef *) GPIOG_BASE)
+#   define GPIOH               ((GPIO_TypeDef *) GPIOH_BASE)
+#endif
+
 #endif /* __STM32F303xE_H */
