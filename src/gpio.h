08515918beeb176c3a323f053db66a9a1684aff6
Brooks Anderson <bilbrobaggins@gmail.com>
2020-07-24 01:15:19 -0400

    Remove implementation specific code.

--- src/gpio.h
+++ src/gpio.h
@@ -10,30 +10,11 @@
 
 #include <stdint.h>
 
-#include "stm32f303xe.h"
+#include "gpio_stm32f303.h"
 
-typedef struct {
-   uint8_t port;
-   uint8_t pin;
-} Gpio_Pin_t;
+#define HW_LINK(func, x)  GPIO_STM32F303_##func(x)
 
-#define GPIO_PIN(port,pin) ((Gpio_Pin_t){ (port), (pin) })
-
-/*!< AHB2 peripherals */
-GPIO_TypeDef GPIO_BASE[8];
-#define GPIO ((GPIO_TypeDef *)GPIO_BASE)
-#define GPIO_PORT_A_IDX (0)
-#define GPIO_PORT_B_IDX (1)
-#define GPIO_PORT_C_IDX (2)
-#define GPIO_PORT_D_IDX (3)
-#define GPIO_PORT_E_IDX (4)
-#define GPIO_PORT_F_IDX (5)
-#define GPIO_PORT_G_IDX (6)
-#define GPIO_PORT_H_IDX (7)
-
-#define STM32F303_PIN_PA5    GPIO_PIN(GPIO_PORT_A_IDX, 5)
-
-#define GPIO_Set(p) (GPIO[(p).port].ODR |= (1 << (p).pin))
-#define GPIO_Clr(p) (GPIO[(p).port].ODR &= ~(1 << (p).pin))
+#define GPIO_Set(x)     HW_LINK(Set, x)
+#define GPIO_Clr(x)     HW_LINK(Clr, x)
 
 #endif /* _GPIO_H */
