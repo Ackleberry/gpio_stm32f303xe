7ec808a20bc8be20cb1f27e7c8c11dc52887691a
Brooks Anderson <bilbrobaggins@gmail.com>
2020-07-27 23:58:16 -0400

    Remove __IO definition

--- inc/stm32f303xe.h
+++ inc/stm32f303xe.h
@@ -3,23 +3,21 @@
 
 #include <stdint.h>
 
-#define     __IO    volatile             /*!< Defines 'read / write' permissions */
-
 /**
   * @brief General Purpose I/O
   */
 typedef struct _GPIO_TypeDef
 {
-  __IO uint32_t MODER;        /*!< GPIO port mode register,               Address offset: 0x00      */
-  __IO uint32_t OTYPER;       /*!< GPIO port output type register,        Address offset: 0x04      */
-  __IO uint32_t OSPEEDR;      /*!< GPIO port output speed register,       Address offset: 0x08      */
-  __IO uint32_t PUPDR;        /*!< GPIO port pull-up/pull-down register,  Address offset: 0x0C      */
-  __IO uint32_t IDR;          /*!< GPIO port input data register,         Address offset: 0x10      */
-  __IO uint32_t ODR;          /*!< GPIO port output data register,        Address offset: 0x14      */
-  __IO uint32_t BSRR;         /*!< GPIO port bit set/reset register,      Address offset: 0x1A */
-  __IO uint32_t LCKR;         /*!< GPIO port configuration lock register, Address offset: 0x1C      */
-  __IO uint32_t AFR[2];       /*!< GPIO alternate function registers,     Address offset: 0x20-0x24 */
-  __IO uint32_t BRR;          /*!< GPIO bit reset register,               Address offset: 0x28 */
+  volatile uint32_t MODER;        /*!< GPIO port mode register,               Address offset: 0x00      */
+  volatile uint32_t OTYPER;       /*!< GPIO port output type register,        Address offset: 0x04      */
+  volatile uint32_t OSPEEDR;      /*!< GPIO port output speed register,       Address offset: 0x08      */
+  volatile uint32_t PUPDR;        /*!< GPIO port pull-up/pull-down register,  Address offset: 0x0C      */
+  volatile uint32_t IDR;          /*!< GPIO port input data register,         Address offset: 0x10      */
+  volatile uint32_t ODR;          /*!< GPIO port output data register,        Address offset: 0x14      */
+  volatile uint32_t BSRR;         /*!< GPIO port bit set/reset register,      Address offset: 0x1A      */
+  volatile uint32_t LCKR;         /*!< GPIO port configuration lock register, Address offset: 0x1C      */
+  volatile uint32_t AFR[2];       /*!< GPIO alternate function registers,     Address offset: 0x20-0x24 */
+  volatile uint32_t BRR;          /*!< GPIO bit reset register,               Address offset: 0x28      */
 } GPIO_TypeDef;
 
 /*!< AHB2 peripherals */
